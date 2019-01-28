d732ede619d2e0fe6288e9c15d69d83eb59fabb7
MasatakaOba <masata_828@icloud.com>
2019-01-29 03:17:51 +0900

    画像アップロード

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20190115142937) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.string   "image"
+    t.binary   "image",      limit: 16777215
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at"
