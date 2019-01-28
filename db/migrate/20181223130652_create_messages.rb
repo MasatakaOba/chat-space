c37622743834c9605b238cdadbc107bd79fe0842
MasatakaOba <masata_828@icloud.com>
2019-01-29 04:32:13 +0900

    Update 20181223130652_create_messages.rb

--- db/migrate/20181223130652_create_messages.rb
+++ db/migrate/20181223130652_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :content
-      t.binary :image, :limit => 2.megabyte
+      t.string :image
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps null: true
