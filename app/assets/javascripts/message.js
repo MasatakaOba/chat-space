c2538c04303f8cfa47ce42aaaca2b446f4d7e6da
MasatakaOba <masata_828@icloud.com>
2019-01-30 20:47:41 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,7 +54,7 @@ $(function(){
       .done(function(data){
          loadMessages.call()
          $('.form__message').val('')
-         $('.form__mask').val('')
+         $('#message_image').val('')
          $('.form__submit').prop('disabled', false);
       })
       .fail(function(){
